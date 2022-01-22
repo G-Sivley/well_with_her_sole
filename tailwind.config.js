5d72a79ab0ad8ad5029eee277f9ba51579ffcee8
Grant Sivley <grant.sivley@gmail.com>
2022-01-22 11:25:59 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "kelly-green-100": "#e3e8e6",
+        "kelly-green-300": "#c7d1cc",
+        "kelly-green-500": "#adbcb5",
+        "kelly-green-700": "#9dafa6",
+        "kelly-green-800": "#81988d",
+        "kelly-green-900": "#5c7067"
+      },
+    },
   },
   plugins: [],
 }
