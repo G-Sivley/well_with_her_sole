e62b45324f94ce71882647d636eeeed6777ea653
Grant Sivley <grant.sivley@gmail.com>
2022-01-22 11:35:18 -0700

    bug fix

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,7 +4,7 @@ import parse from "html-react-parser";
 
 import { getComments } from "../services";
 
-const Comments = ({ slug }) => {
+const Comments = ({ slug }: { slug: any }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
@@ -30,8 +30,8 @@ const Comments = ({ slug }) => {
                 {` `}
                 {moment(comment.createdAt).format("MMM DD, YYYY HH:MM")}
               </p>
-              <p className="whitespace-pree-line text-gray-600 w-full">{parse(comment.comment)}
-
+              <p className="whitespace-pree-line text-gray-600 w-full">
+                {parse(comment.comment)}
               </p>
             </div>
           ))}
